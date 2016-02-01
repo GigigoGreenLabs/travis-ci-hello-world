288c362def86f5ba13c60959f93e1c1b5e0ce90a
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2016-02-01 21:49:03 +0100

    added travis file

--- app/src/test/java/com/gigigo/travis/ExampleUnitTest.java
+++ app/src/test/java/com/gigigo/travis/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.gigigo.travis;
 
+import java.lang.System;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,5 +11,18 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
   @Test public void addition_isCorrect() throws Exception {
     assertEquals(4, 2 + 2);
+  }
+
+  @Test public void second_addition_isCorrect() throws Exception {
+    System.out.println("------>>>TEST ARE BEING PASSED<<<-----");
+    assertEquals(5, 2 + 3);
+  }
+
+  @Test public void third_addition_isCorrect() throws Exception {
+    assertEquals(2, 1 + 1);
+  }
+
+  @Test public void fourth_addition_isCorrect() throws Exception {
+    assertEquals(8, 4 + 4);
   }
 }
